3016d4060b0482d08a5d31dd5864e6b53757e634
jiawumorina@gmail.com <jiawumorina@gmail.com@ae91eb80-11e0-a7bc-8760-f5734d9dd8ff>
2013-10-12 09:26:54 +0000

    JMS测试用例

---
+++ trunk/wprojectframework/src/test/java/com/wprojectframework/jms/TestJMSSender.java
@@ -0,0 +1,143 @@
+package com.wprojectframework.jms;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.wprojectframework.core.BeanHandler;
+
+/**
+ * JMS发送器测试用例
+ * @author lenovo
+ *
+ */
+public class TestJMSSender {
+	
+	/**
+	 * 队列发送器
+	 */
+	Sender queueSender;
+	
+	/**
+	 * 主题发送器
+	 */
+	Sender topicSender;
+	
+	/**
+	 * before
+	 */
+	@Before
+	public void before(){
+		queueSender = BeanHandler.getBean("queueSender");
+		topicSender = BeanHandler.getBean("topicSender");
+	}
+	
+	/**
+	 * 队列发送消息测试
+	 */
+//	@Test
+	public void testQueueSender(){
+		queueSender.send("dxh_tfels_queue", "first test message");
+		
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("1", "hello");
+		map.put("2", new ArrayList<String>(2));
+//		queueSender.send("queue_2", map);
+		
+		User user = new User();
+		user.setId(1);
+		user.setUserName("dsms");
+		user.setPassword("fjslkfja");
+//		queueSender.send("queue_3", user);
+		
+	}
+	
+	/**
+	 * 主题发送消息测试
+	 */
+	@Test
+	public void testTopicSender(){
+		topicSender.send("first test message");
+//		topicSender.send("first test message");
+//		Map<String,Object> map = new HashMap<String, Object>();
+//		map.put("1", "hello");
+//		map.put("2", new ArrayList<String>(2));
+//		topicSender.send("topic_2", map);
+//		
+//		User user = new User();
+//		user.setId(1);
+//		user.setUserName("dsms");
+//		user.setPassword("fjslkfja");
+//		topicSender.send("topic_3", user);
+		
+	}
+	
+//	/**
+//	 * 
+//	 */
+//	@After
+//	public void after(){
+//		queueSender.closeSession();
+//		topicSender.closeSession();
+//		
+//	}
+}
+
+class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4727557233703970529L;
+
+	private int id;
+	
+	private String userName;
+	
+	private String password;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
