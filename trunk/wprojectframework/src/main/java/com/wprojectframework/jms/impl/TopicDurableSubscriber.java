1fca6e41c36a0c5797d7b1c4f8f46eaa47ff6a83
jiawumorina@gmail.com <jiawumorina@gmail.com@ae91eb80-11e0-a7bc-8760-f5734d9dd8ff>
2013-10-15 06:13:52 +0000

    新增主题持久订阅者，可通过IOC注入订阅名称

---
+++ trunk/wprojectframework/src/main/java/com/wprojectframework/jms/impl/TopicDurableSubscriber.java
@@ -0,0 +1,49 @@
+package com.wprojectframework.jms.impl;
+
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Topic;
+import com.wprojectframework.jms.JMSAbstractReceiver;
+
+/**
+ * 
+ * @class TopicDurableSubscriber.java
+ * @author wujia
+ * @date 2013-10-15
+ * @version v1.0
+ * @todo
+ * 主题持久化消息订阅者
+ */
+public class TopicDurableSubscriber extends JMSAbstractReceiver{
+	
+	/**
+	 * 订阅名称
+	 */
+	private String subscriber;
+	
+	/**
+	 * @return the subscriber
+	 */
+	public String getSubscriber() {
+		return subscriber;
+	}
+
+	/**
+	 * @param subscriber the subscriber to set
+	 */
+	public void setSubscriber(String subscriber) {
+		this.subscriber = subscriber;
+	}
+
+
+
+	@Override
+	protected MessageConsumer getConsumer() throws JMSException {
+		if(this.destination == null){
+			return null;
+		}
+		Topic topic = getSession().createTopic(this.destination);
+		return getSession().createDurableSubscriber(topic, subscriber);
+	}
+
+}
